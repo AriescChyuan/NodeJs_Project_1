0fe71c9aebb93423b9b4d6c912ccd391f8637de5
Bruce <jetliayu@gmail.com>
2020-12-02 15:29:18 +0800

    heroku push

--- config/development_config.js
+++ config/development_config.js
@@ -2,15 +2,15 @@ require('dotenv').config()
 //為了不在程式碼的各處加入新的環境變數，
 //而想要集中在 .env 檔中管理它，所以有大神寫了 dotenv , 可以在程式一開始就載入所有的環境變數。
 
-module.exports = {
-  mysql: {
-      host: process.env.HOST,
-      user: process.env.DATABASE_USER,
-      password: process.env.DATABASE_PASSWORD,
-      database: process.env.DATABASE
-    },
-    secret:process.env.MY_SECRET
-}
+// module.exports = {
+//   mysql: {
+//       host: process.env.HOST,
+//       user: process.env.DATABASE_USER,
+//       password: process.env.DATABASE_PASSWORD,
+//       database: process.env.DATABASE
+//     },
+//     secret:process.env.MY_SECRET
+// }
 
 
 module.exports = {
